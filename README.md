581edea40d48b2fdf1c99e882580c60ae1dadfc1
Nabos <nabos@glargh.fr>
2022-09-29 14:08:22 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+![Screenshot](screenshot.png "In-game screenshot")
+
+Don't forget to edit the controls in main.src in the `player_movements` function
---
+++ src/main.rs
@@ -0,0 +1,259 @@
+use std::f32::consts::PI;
+
+use bevy::prelude::*;
+use rand::Rng;
+
+#[derive(Component)]
+struct Bullet;
+
+#[derive(Component)]
+struct Collider;
+
+#[derive(Default)]
+struct CollisionEvent;
+
+#[derive(Default)]
+struct Player(SpriteBundle);
+
+#[derive(Component)]
+struct Velocity(Vec2);
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+enum AppState {
+    Start,
+    Playing,
+}
+
+#[derive(Default)]
+struct Game {
+    player: Option<Entity>,
+    score: u128,
+    timer: Timer,
+}
+
+fn main() {
+    App::new()
+        .init_resource::<Game>()
+        .add_plugins(DefaultPlugins)
+        .add_state(AppState::Start)
+        .add_startup_system(setup)
+        .add_system_set(SystemSet::on_update(AppState::Start).with_system(start))
+        .add_system_set(SystemSet::on_enter(AppState::Playing).with_system(clean))
+        .add_system_set(
+            SystemSet::on_update(AppState::Playing)
+                .with_system(tick)
+                .with_system(score_update.after(tick))
+                .with_system(bullet_spawn.after(tick))
+                .with_system(bullet_movements.after(tick))
+                .with_system(player_movements.after(tick))
+                .with_system(check_for_collisions.after(tick)),
+        )
+        .add_system(bevy::window::close_on_esc)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMut<Game>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+
+    game.player = Some(
+        commands
+            .spawn()
+            .insert_bundle(SpriteBundle {
+                transform: Transform {
+                    translation: Vec3::new(0., 0., 0.),
+                    scale: Vec3::new(10., 10., 10.),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: Color::rgb(0.9, 0., 0.),
+                    ..default()
+                },
+                ..default()
+            })
+            .insert(Collider)
+            .id(),
+    );
+
+    game.timer = Timer::from_seconds(0.2, true);
+
+    commands.spawn_bundle(
+        TextBundle::from_section(
+            // Accepts a `String` or any type that converts into a `String`, such as `&str`
+            "Score: 0",
+            TextStyle {
+                font: asset_server.load("DejaVuSans.ttf"),
+                font_size: 50.0,
+                color: Color::WHITE,
+            },
+        )
+        .with_text_alignment(TextAlignment::TOP_CENTER), // Set the alignment of the Text
+    );
+}
+
+fn start(mut state: ResMut<State<AppState>>, keyboard_input: Res<Input<KeyCode>>) {
+    if keyboard_input.pressed(KeyCode::Space) {
+        state.set(AppState::Playing).unwrap();
+    }
+}
+
+fn clean(
+    mut commands: Commands,
+    mut game: ResMut<Game>,
+    components: Query<(Entity, &Bullet), With<Bullet>>,
+) {
+    for (entity, _bullet) in &components {
+        commands.entity(entity).despawn_recursive();
+    }
+    game.score = 0;
+}
+
+fn tick(time: Res<Time>, mut game: ResMut<Game>) {
+    game.timer.tick(time.delta()).just_finished();
+    game.score += time.delta().as_millis();
+}
+
+fn score_update(game: Res<Game>, mut query: Query<&mut Text>) {
+    for mut text in &mut query {
+        text.sections[0].value = game.score.to_string();
+    }
+}
+
+fn bullet_spawn(
+    mut commands: Commands,
+    game: ResMut<Game>,
+    asset_server: Res<AssetServer>,
+    mut transforms: Query<&mut Transform>,
+) {
+    if game.timer.just_finished() {
+        let player_transform = transforms.get_mut(game.player.unwrap()).unwrap();
+        let rand_angle = rand::thread_rng().gen_range(0..(PI * 2000.) as u32) as f32 / 1000.;
+        let rand_quad = Quat::from_rotation_z(rand_angle);
+        let spawn_location = rand_quad * Vec3::new(1., 0., 0.).normalize() * 700.;
+        let diff = player_transform.translation - spawn_location;
+        let angle = diff.y.atan2(diff.x);
+        commands
+            .spawn_bundle(SpriteBundle {
+                texture: asset_server.load("bullet.png"),
+                transform: Transform {
+                    translation: spawn_location,
+                    rotation: Quat::from_rotation_z(angle),
+                    scale: Vec3::new(0.1, 0.1, 0.1),
+                    ..default()
+                },
+                sprite: Sprite {
+                    color: Color::rgb(0.9, 0.9, 0.2),
+                    ..default()
+                },
+                ..default()
+            })
+            .insert(Bullet)
+            .insert(Velocity(
+                Vec2::new(diff.x, diff.y).normalize() * Vec2::new(1000., 1000.),
+            ))
+            .insert(Collider);
+    }
+}
+
+fn bullet_movements(
+    time: Res<Time>,
+    mut commands: Commands,
+    mut bullet_velocities: Query<(Entity, &Velocity, &mut Transform), With<Bullet>>,
+) {
+    for (entity, velocity, mut transform) in &mut bullet_velocities {
+        transform.translation.x += velocity.0.x * time.delta_seconds();
+        transform.translation.y += velocity.0.y * time.delta_seconds();
+
+        const LIMIT: f32 = 1000.;
+        if transform.translation.x > LIMIT || transform.translation.x < -LIMIT {
+            commands.entity(entity).despawn_recursive();
+        }
+        if transform.translation.y > LIMIT || transform.translation.y < -LIMIT {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+fn player_movements(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    game: ResMut<Game>,
+    mut transforms: Query<&mut Transform>,
+) {
+    const PLAYER_SPEED: f32 = 200.;
+    let mut player_transform = transforms.get_mut(game.player.unwrap()).unwrap();
+    if keyboard_input.pressed(KeyCode::E) {
+        player_transform.translation.x += PLAYER_SPEED * time.delta_seconds();
+    };
+    if keyboard_input.pressed(KeyCode::U) {
+        player_transform.translation.x -= PLAYER_SPEED * time.delta_seconds();
+    };
+    if keyboard_input.pressed(KeyCode::P) {
+        player_transform.translation.y += PLAYER_SPEED * time.delta_seconds();
+    };
+    if keyboard_input.pressed(KeyCode::I) {
+        player_transform.translation.y -= PLAYER_SPEED * time.delta_seconds();
+    };
+}
+
+fn check_for_collisions(
+    mut state: ResMut<State<AppState>>,
+    game: ResMut<Game>,
+    assets: Res<Assets<Image>>,
+    transforms: Query<&Transform>,
+    collider_query: Query<(Entity, &Handle<Image>, &Transform, &Collider), With<Bullet>>,
+) {
+    let player_transform = transforms.get(game.player.unwrap()).unwrap();
+
+    for (_collider_entity, texture_handle, transform, _collider) in &collider_query {
+        if let Some(texture) = assets.get(texture_handle) {
+            let size = texture.size() * transform.scale.truncate();
+            if collide_with_rotation(
+                player_transform.translation.truncate(),
+                transform.translation.truncate(),
+                size,
+                transform.rotation,
+            ) {
+                println!("Score was: {}", game.score);
+                state.overwrite_set(AppState::Start).unwrap();
+            }
+        }
+    }
+}
+
+// ============== COLLISION DETECTION ==============
+
+fn collide_with_rotation(
+    point: Vec2,
+    rectangle_position: Vec2,
+    rectangle_size: Vec2,
+    rectangle_rotation: Quat,
+) -> bool {
+    let point_1 = Vec3::new(-rectangle_size.x / 2., rectangle_size.y / 2., 0.);
+    let point_2 = Vec3::new(-rectangle_size.x / 2., -rectangle_size.y / 2., 0.);
+    let point_3 = Vec3::new(rectangle_size.x / 2., rectangle_size.y / 2., 0.);
+    let point_4 = Vec3::new(rectangle_size.x / 2., -rectangle_size.y / 2., 0.);
+    is_point_inside_rectangle(
+        point,
+        rectangle_rotation.mul_vec3(point_1).truncate() + rectangle_position,
+        rectangle_rotation.mul_vec3(point_2).truncate() + rectangle_position,
+        rectangle_rotation.mul_vec3(point_3).truncate() + rectangle_position,
+        rectangle_rotation.mul_vec3(point_4).truncate() + rectangle_position,
+    )
+}
+
+fn is_point_inside_rectangle(t: Vec2, p1: Vec2, p2: Vec2, p3: Vec2, p4: Vec2) -> bool {
+    is_point_inside_triangle(t, p1, p2, p3) || is_point_inside_triangle(t, p2, p4, p3)
+}
+
+fn is_point_inside_triangle(t: Vec2, p1: Vec2, p2: Vec2, p3: Vec2) -> bool {
+    let area_ref = area(p1, p2, p3);
+
+    let area_1 = area(p1, p2, t);
+    let area_2 = area(p1, t, p3);
+    let area_3 = area(t, p2, p3);
+    area_ref >= area_1 + area_2 + area_3
+}
+
+fn area(p1: Vec2, p2: Vec2, p3: Vec2) -> f32 {
+    return ((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2.).abs();
+}
